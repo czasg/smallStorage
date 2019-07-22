155a0a7ba8139ae8f52f418f1e1117d01134f2d0
cza <czasg0.0>
2019-07-22 14:57:42 +0800

    add code

--- 需求库/python_pillow/pillow_generate_pictures_func.py
+++ 需求库/python_pillow/pillow_generate_pictures_func.py
@@ -25,18 +25,24 @@ def rndColor2():
 width = 60 * 4
 height = 60
 
+#  生成图片
 img = Image.new('RGB', (width, height), (255, 255, 255))
 
+# 创建Font对象:
 font = ImageFont.truetype('C:\Windows\Fonts\Arial.ttf', 36)
 
+# 创建Draw对象:
 draw = ImageDraw.Draw(img)
 
+# 填充每个像素:
 for x in range(width):
     for y in range(height):
         draw.point((x, y), fill=rndColor())
 
+# 输出文字:
 for t in range(4):
     draw.text((60 * t + 10, 10), rndChar(), font=font, fill=rndColor2())
 
+# 模糊:
 img = img.filter(ImageFilter.BLUR)
 img.save('test3.jpg', 'jpeg')
